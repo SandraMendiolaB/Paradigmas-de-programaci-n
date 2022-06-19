e548aba7740c909bc2ab42be8aba414f5db26556
SandraMendiolaB <99694904+SandraMendiolaB@users.noreply.github.com>
2022-06-19 14:51:07 -0500

    Add files via upload

---
+++ lab 7/abrir.py
@@ -0,0 +1,20 @@
+#===========================#
+# Abrir un Archivo de Texto #
+#===========================#
+#===========================================#
+# EN ESTE PROGRAMA LO QUE SE BUSCA ES ABRIR #
+# UN ARCHIVO EXISTENTE Y LEERLO             #
+#===========================================#
+print("====================")
+print("¡Bienvenido Usuario!")
+print("====================")
+
+nombre = input("Ingrese el nombre de su archivo con su extension: ")
+
+try:
+    archivo = open(nombre, 'rb')
+except OSError:
+    print("Error al abrir el archivo: ", nombre)
+    exit()
+with archivo:
+    print(archivo.read())
